602724b8255348dccc9d369e27595ad3642cfe43
usany <ckd_qja@naver.com>
2026-08-06 16:47:24 +0900

    cern

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,9 +1,5 @@
 export const EPISODES = [
-  { title: "Episode 1: Getting Started", podcast: "Tech Talk Daily", duration: "18:42", url: "https://www.soundhelix.com/sounds/mp3/SoundHelix-Song-1.mp3" },
-  { title: "Episode 2: Advanced Topics", podcast: "Tech Talk Daily", duration: "25:15", url: "https://www.soundhelix.com/sounds/mp3/SoundHelix-Song-2.mp3" },
-  { title: "Episode 3: Q&A Session", podcast: "Tech Talk Daily", duration: "22:30", url: "https://www.soundhelix.com/sounds/mp3/SoundHelix-Song-3.mp3" },
-  { title: "Episode 1: Introduction", podcast: "Code Weekly", duration: "15:45", url: "https://www.soundhelix.com/sounds/mp3/SoundHelix-Song-4.mp3" },
-  { title: "Episode 2: Best Practices", podcast: "Code Weekly", duration: "29:10", url: "https://www.soundhelix.com/sounds/mp3/SoundHelix-Song-5.mp3" },
+  { title: "Newsfront", podcast: "News Podcast", duration: "0:00", url: "/Newsfront.mp3" },
 ]
 
 export function formatTime(seconds) {
