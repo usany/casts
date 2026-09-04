082502936778f088e189850cfcaebd6825abeff3
usany <ckd_qja@naver.com>
2026-09-04 17:55:30 +0900

    cern

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Welcome!"', () => {
+      expect(appController.getWelcome()).toBe('Welcome!');
+    });
+  });
+});
